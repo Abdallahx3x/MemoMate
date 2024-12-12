ae3eb6d9f421ebbfe383c6b4a50329d468e32c1d
Abdallah Sayed <abdullahsayed3355@gmail.com>
2024-12-12 22:18:57 +0200

    refactor home and details layouts and make min sdk 23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.abdo.memomate"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
